2f1d3c8d1f8f9f0d5d554a18eb0aea96a8d11b04
Divin Fiston ISHIMWE <divinfiston1@gmail.com>
2024-02-22 18:44:03 +0200

    activity log IP address

--- frappe/core/doctype/activity_log/activity_log.py
+++ frappe/core/doctype/activity_log/activity_log.py
@@ -29,7 +29,7 @@ class ActivityLog(Document):
 			self.status = "Linked"
 	def set_ip_address(self):
 		if self.operation in ("Login", "Logout"):
-			self.ip_address = frappe.get_request_header('Forwarded-For')
+			self.ip_address = frappe.get_request_header()
 
 def on_doctype_update():
 	"""Add indexes in `tabActivity Log`"""
